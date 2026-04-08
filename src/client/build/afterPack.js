cbbf07be93aac29179fd39417f25b19f9a48035a
Kanishk Gandhi <kanishk050296@gmail.com>
2026-04-08 00:10:10 -0700

    icon, build

---
+++ src/client/build/afterPack.js
@@ -0,0 +1,11 @@
+const { execSync } = require("child_process");
+const path = require("path");
+
+exports.default = async function (context) {
+  const appPath = path.join(
+    context.appOutDir,
+    `${context.packager.appInfo.productFilename}.app`
+  );
+  console.log("[afterPack] ad-hoc signing:", appPath);
+  execSync(`codesign --force --deep --sign - "${appPath}"`, { stdio: "inherit" });
+};
